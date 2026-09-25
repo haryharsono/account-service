385036586f5f13861363b3c4a58344c0db0957a3
haryharsono <hary.harsono@ist.id>
2026-09-26 05:31:53 +0700

    [fix] init

---
+++ src/main/java/com/haryharsono/ServiceAccountApplication.java
@@ -0,0 +1,13 @@
+package com.haryharsono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAccountApplication.class, args);
+	}
+
+}
